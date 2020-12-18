22b899bcf08266c21a988405e57b59af75804a46
GeoJulien <dev@ingeoveritas.com>
2020-12-18 21:26:18 +0100

    Handle latest option

--- qgispluginci/changelog.py
+++ qgispluginci/changelog.py
@@ -14,7 +14,7 @@ class ChangelogParser:
     def has_changelog():
         return isfile('CHANGELOG.md')
 
-    def __init__(self, regexp):
+    def __init__(self, regexp: str):
         self.regexp = regexp
 
     def _parse(self):
@@ -26,7 +26,7 @@ class ChangelogParser:
 
         return re.findall(self.regexp, content, flags=re.MULTILINE | re.DOTALL)
 
-    def last_items(self, count):
+    def last_items(self, count: int) -> str:
         """Content to add in the metadata.txt.
 
         :param count: Maximum number of tags to include in the file.
@@ -45,9 +45,12 @@ class ChangelogParser:
             output += '\n'
         return output
 
-    def content(self, tag):
+    def content(self, tag: str) -> str:
         """Content to add in a release according to a tag."""
         changelog_content = self._parse()
+        if tag == "latest":
+            for version, date, items in changelog_content[:1]:
+                return items.strip()
         for version, date, items in changelog_content:
             if version == tag:
                 return items.strip()
