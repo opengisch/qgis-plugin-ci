9a18c9679e09f71dc382873bfde5014a2f29b7bf
Denis Rouzaud <denis.rouzaud@gmail.com>
2021-02-10 21:40:54 +0100

    Revert "Changelog: add latest"

--- qgispluginci/changelog.py
+++ qgispluginci/changelog.py
@@ -14,19 +14,19 @@ class ChangelogParser:
     def has_changelog():
         return isfile('CHANGELOG.md')
 
-    def __init__(self, regexp: str):
+    def __init__(self, regexp):
         self.regexp = regexp
 
     def _parse(self):
         if not self.has_changelog():
             return ''
 
-        with open('CHANGELOG.md', "r") as f: 
-            content = f.read()
-
+        f = open('CHANGELOG.md', "r")
+        content = f.read()
+        f.close()
         return re.findall(self.regexp, content, flags=re.MULTILINE | re.DOTALL)
 
-    def last_items(self, count: int) -> str:
+    def last_items(self, count):
         """Content to add in the metadata.txt.
 
         :param count: Maximum number of tags to include in the file.
@@ -45,12 +45,9 @@ class ChangelogParser:
             output += '\n'
         return output
 
-    def content(self, tag: str) -> str:
+    def content(self, tag):
         """Content to add in a release according to a tag."""
         changelog_content = self._parse()
-        if tag == "latest":
-            for version, date, items in changelog_content[:1]:
-                return items.strip()
         for version, date, items in changelog_content:
             if version == tag:
                 return items.strip()
