90ca81f5c8546302941adf92dcc671ca0974a0c9
GeoJulien <dev@ingeoveritas.com>
2021-03-31 20:56:21 +0200

    Improve the has_changelog method to allow passing a folder

--- qgispluginci/changelog.py
+++ qgispluginci/changelog.py
@@ -1,25 +1,83 @@
-"""Changelog parser.
+#! python3  # noqa E265
 
-Following nearly https://keepachangelog.com/en/1.0.0/
+"""
+    Changelog parser. Following <https://keepachangelog.com/en/1.0.0/>.
 """
 
+# ############################################################################
+# ########## Libraries #############
+# ##################################
+
+# standard library
+import logging
 import re
-from os.path import isfile
+from pathlib import Path
+from typing import Union
+
+# ############################################################################
+# ########## Globals #############
+# ################################
+
+logger = logging.getLogger(__name__)
+
+# ############################################################################
+# ########## Classes #############
+# ################################
 
 
 class ChangelogParser:
-    @staticmethod
-    def has_changelog():
-        return isfile("CHANGELOG.md")
+
+    CHANGELOG_FILEPATH: Union[Path, None] = None
+
+    @classmethod
+    def has_changelog(cls, parent_folder: Union[Path, str] = Path(".")) -> bool:
+        """Check if a changelog file exists within the parent folder. If it does, \
+        it returns True and the file path is stored as class attribute. If not, it \
+        returns False and the class attribute is reset to None.
+
+        Args:
+            parent_folder (Union[Path, str], optional): parent folder where to look \
+                for a `CHANGELOG.md` file. Defaults to Path(".").
+
+        Raises:
+            FileExistsError: if the parent_folder path doesn't exist
+            TypeError: if the path is not a folder but a path
+
+        Returns:
+            bool: True if a CHANGELOG.md exists within the parent_folder
+        """
+        # reset stored path as class attribute
+        cls.CHANGELOG_FILEPATH = None
+
+        # ensure using pathlib.Path
+        if isinstance(parent_folder, str):
+            parent_folder = Path(parent_folder)
+
+        # check if the folder exists
+        if not parent_folder.exists():
+            raise FileExistsError(
+                f"Parent folder doesn't exist: {parent_folder.resolve()}"
+            )
+        # check if path is a folder
+        if not parent_folder.is_dir():
+            raise TypeError(f"Path is not a folder: {parent_folder.resolve()}")
+
+        # build, check and store the changelog path
+        cls.CHANGELOG_FILEPATH = parent_folder / "CHANGELOG.md"
+        if cls.CHANGELOG_FILEPATH.is_file():
+            logger.info(f"Changelog file used: {cls.CHANGELOG_FILEPATH.resolve()}")
+
+        return cls.CHANGELOG_FILEPATH.is_file()
 
     def __init__(self, regexp: str):
         self.regexp = regexp
+        self.has_changelog()
 
     def _parse(self):
-        if not self.has_changelog():
+        if not self.CHANGELOG_FILEPATH:
             return ""
 
-        with open("CHANGELOG.md", "r") as f:
+        with self.CHANGELOG_FILEPATH.open(mode="r", encoding="UTF8") as f:
             content = f.read()
 
         return re.findall(self.regexp, content, flags=re.MULTILINE | re.DOTALL)
