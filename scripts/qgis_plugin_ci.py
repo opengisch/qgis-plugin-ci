8a91dc374cf43fa2de713e0c611401e5025fd9dc
Etienne Trimaille <gustrimaille@yahoo.fr>
2022-01-20 07:34:00 +0100

    Fix regression when using the changelog command without a configuration file

--- scripts/qgis_plugin_ci.py
+++ scripts/qgis_plugin_ci.py
@@ -135,15 +135,24 @@ def main():
     exit_val = 0
 
     if os.path.isfile(".qgis-plugin-ci"):
+        # We read the .qgis-plugin-ci file
         arg_dict = yaml.safe_load(open(".qgis-plugin-ci"))
     else:
         config = configparser.ConfigParser()
         config.read("setup.cfg")
-        if "qgis-plugin-ci" not in config.sections():
-            raise ConfigurationNotFound(
-                ".qgis-plugin-ci or setup.cfg with a 'qgis-plugin-ci' section have not been found."
-            )
-        arg_dict = dict(config.items("qgis-plugin-ci"))
+        if "qgis-plugin-ci" in config.sections():
+            # We read the setup.cfg file
+            arg_dict = dict(config.items("qgis-plugin-ci"))
+        else:
+            # We don't have either a .qgis-plugin-ci or a setup.cfg
+            if args.command == "changelog":
+                # but for the "changelog" sub command, the config file is not required, we can continue
+                arg_dict = dict()
+            else:
+                raise ConfigurationNotFound(
+                    ".qgis-plugin-ci or setup.cfg with a 'qgis-plugin-ci' section have not been found."
+                )
+
     parameters = Parameters(arg_dict)
 
     # CHANGELOG
